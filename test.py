324e9a07363813d8d902616b0e696e010486b2ca
Alec Newman <alecnwmn904@gmail.com>
2017-03-29 11:27:58 -0600

    Added mmap to python test

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
 import csv
 import datetime
 import itertools
+import contextlib
+import mmap
+
+@contextlib.contextmanager
+def csv_reader(filename):
+    with open('test.csv') as f:
+        data = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+        try:
+            def generator():
+                start = 0
+
+                while True:
+                    eol = data.find(b'\n', start)
+                    if eol == -1:
+                        break
+                    yield data[start:eol].split(b',')
+                    start = eol + 1
+
+            yield generator()
+        finally:
+            data.close()
 
 n = 1000000
 with open('test.csv') as f:
@@ -10,5 +31,12 @@ with open('test.csv') as f:
     for line in itertools.islice(r, n):
         pass
     end = datetime.datetime.now()
+print('python csv\t%.0f ns / op' % (1e9 * (end - start).total_seconds() / n))
 
-print('%.0f ns / op' % (1e9 * (end - start).total_seconds() / n))
+n = 1000000
+with csv_reader('test.csv') as r:
+    start = datetime.datetime.now()
+    for line in itertools.islice(r, n):
+        pass
+    end = datetime.datetime.now()
+print('python mmap-csv\t%.0f ns / op' % (1e9 * (end - start).total_seconds() / n))
